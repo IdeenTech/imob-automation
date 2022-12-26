74fc272231fe508c5eaa8bf289a409d76ddfc502
[gabriel freire] <gabriel.freire-ext@viavarejo.com.br>
2022-12-26 07:44:06 -0300

    ajuste de BankAdress e cenarios de RegisterBuilding

--- src/test/java/com/automation/imob/ImobApplicationTests.java
+++ src/test/java/com/automation/imob/ImobApplicationTests.java
@@ -4,10 +4,16 @@ import com.automation.imob.components.MethodRest;
 import com.automation.imob.components.config.EndpointConfig;
 import com.automation.imob.components.util.DataFaker;
 import com.automation.imob.config.ConfigParams;
+import com.automation.imob.config.ImobFileJson;
+import com.automation.imob.config.ImobPath;
 import io.restassured.response.Response;
 import lombok.Getter;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 @SpringBootTest
 @Getter
@@ -35,5 +41,52 @@ public class ImobApplicationTests {
     protected DataFaker getDataFaker(){
         return DataFaker.getInstance();
     }
+    public EndpointConfig getEndpointConfig(String path) {
+        EndpointConfig endpointConfig = new EndpointConfig();
+        endpointConfig.addHeadersJson(getAccessToken());
+        endpointConfig.setUrl(ConfigParams.HOST.concat(path));
+        return endpointConfig;
+    }
+    protected Response createBankAddres(String DomicRefExterna) throws IOException {
+        Integer banco = getDataFaker().getNumberCharacters(3);
+        Integer agencia = getDataFaker().getNumberCharacters(4);
+        Integer conta = getDataFaker().getNumberCharacters(6);
+
+        //Create dynamics variables
+        HashMap<String, Object> mapValues = new HashMap<>();
+        mapValues.put("referenciaExterna", DomicRefExterna);
+        mapValues.put("banco", banco);
+        mapValues.put("agencia", agencia);
+        mapValues.put("conta", conta);
+
+        // Create Request
+        EndpointConfig endpointConfig = getEndpointConfig(ImobPath.PATH_BANK_ADDRESS);
+        endpointConfig.setBody(endpointConfig.alterValuesInJsonBody(ImobFileJson.PATH_JSON_BANK_ADDRESS_SAVE, mapValues));
+
+
+        return MethodRest.callPost(endpointConfig);
+    }
+
+
+    @Test
+    protected Response CreateBuilding(String DomicRefExterna, String EmpreeRefExternaProjeto) throws IOException {
+       Integer cns = getDataFaker().getNumberCharacters(6);
+       Integer registrationNumber = getDataFaker().getNumberCharacters(7);
+
+       createBankAddres(DomicRefExterna);
+
+        HashMap<String, Object> mapValuesBuilding = new HashMap<>();
+        mapValuesBuilding.put("referenciaExternaProjeto", EmpreeRefExternaProjeto);
+        mapValuesBuilding.put("domicilioBancario", DomicRefExterna);
+        mapValuesBuilding.put("cns", cns);
+        mapValuesBuilding.put("numeroMatricula", registrationNumber);
+
+        // Create Request
+        EndpointConfig endpointConfig = getEndpointConfig(ImobPath.PATH_BUILDING);
+        endpointConfig.setBody(endpointConfig.alterValuesInJsonArrayBody(ImobFileJson.PATH_JSON_BUILDING_SAVE, mapValuesBuilding));
+
+        // Call endpoint
+        return MethodRest.callPost(endpointConfig);
+        }
 
 }
